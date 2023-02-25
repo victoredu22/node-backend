949831526c99c7e0fe1512f5d2f6e774f28761f7
victor curilao <victor.curilao@lablab.cl>
2023-02-24 23:54:14 -0300

    make it better

---
+++ Routes/index.ts
@@ -0,0 +1,48 @@
+import { Router, Request, Response } from "express";
+
+// import UserController from "../Controllers/User.controller";
+// import validateUser from "../validators/User.validator";
+// import validateBank from "../validators/Bank.validator";
+import { body, param, query, validationResult } from "express-validator";
+// import { validate } from "../middleware/rejectInvalid";
+
+// import BankController from "../Controllers/Bank.controller";
+// import TranferController from "../Controllers/Tranfer.controller";
+// import PayeeController from "../Controllers/Payee.controller";
+
+const router = Router();
+
+
+router.get('/', (req: Request, res: Response) => {
+  res.send('Code with Rico. Ready to run on Heroku.');
+});
+
+// router.post(
+//   "/user",
+//   validate(validateUser.validateCreate),
+//   UserController.CreateUser
+// );
+// router.get("/user", UserController.Index);
+
+
+// router.post(
+//   "/payees",
+//   validate(validateUser.validateCreate),
+//   PayeeController.Create
+// );
+// router.get("/payees", PayeeController.Index);
+// router.get("/payees/:email", PayeeController.Get);
+
+
+
+// router.post(
+//   "/bank",
+//   validate(validateBank.validateCreate),
+//   BankController.Create
+// );
+// router.get("/bank", BankController.Index);
+
+
+// router.post("/tranfer", TranferController.createTranfer);
+// router.get("/tranfer", TranferController.Index);
+export default router;
--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
 import express, { Express, Request, Response } from 'express';
 import {config} from "./config";
+import cors = require("cors");
+import bodyParser from "body-parser";
+import routes from "./Routes/index";
 
 const app: Express = express();
+app.use(cors<Request>());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Code with Rico. Ready to run on Heroku.');
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api", routes);
+
 
 app.listen(config.server.port, () => {
     return console.log(`[server]: Server is running on ${config.server.port}`);
